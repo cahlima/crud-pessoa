e522ca798e84268aee837a7b8db8e69e59b3c419
AcaciaLima <caciabad@gmail.com>
2023-08-19 11:10:50 -0300

    pessoaservice

--- src/app/pessoa/services/pessoa.service.ts
+++ src/app/pessoa/services/pessoa.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Pessoa } from 'src/app/shared/models/pessoa.model';
+
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,13 @@ import { Injectable } from '@angular/core';
 export class PessoaService {
 
   constructor() { }
+
+  listarTodos():Pessoa [] {
+    const pessoas = localStorage [LS_CHAVE];
+    // Precisa de um condicional, pois retorna undefined se a chava nao existe
+    return  pessoas ? JSON.parse(pessoas) : [];
+
+
+
+  }
 }
